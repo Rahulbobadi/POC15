2683c396082c0b73a12cb39273d0f7f2084b81c4
Rahulbobadi <rahul.bobadi@gmail.com>
2026-05-14 15:37:25 +0530

    updated supporting files

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
-resource "azurerm_resource_group" "aks_rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-dev-cluster"
   location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
+  resource_group_name = var.resource_group_name
   dns_prefix          = "aksdev"
 
   default_node_pool {
